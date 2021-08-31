0cbc34c276c7bb4d87806e0f3c1e9a7c87127ab9
DatRandomBo1 <75739399+DatRandomBo1@users.noreply.github.com>
2021-08-31 10:24:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song = "";
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload() {
 song = loadSound("Stay.mp3");
 }
@@ -10,6 +13,13 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Inserted Injecting Virus');
 }
 
 function draw() {
@@ -18,4 +28,19 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1)
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " + leftWristX +"LeftWristY = "+ leftWristY);
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("RightWristX = " + rightWristX +"RightWristY = "+ rightWristY);
+    }
 }
